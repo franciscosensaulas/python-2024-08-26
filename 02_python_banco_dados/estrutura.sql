d75491c95cb81fa200537fd527ee18a8a7e3712c
Francisco Lucas Janesch Lange Sens <franciscosensaulas@gmail.com>
2024-09-23 19:58:22 -0300

    Curso Python II Aula 01 2024-09-23

--- 02_python_banco_dados/estrutura.sql
+++ 02_python_banco_dados/estrutura.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema dev_motors
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema dev_motors
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `dev_motors` DEFAULT CHARACTER SET utf8 ;
+USE `dev_motors` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`marcas`
+-- Table `dev_motors`.`marcas`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`marcas` (
+CREATE TABLE IF NOT EXISTS `dev_motors`.`marcas` (
   `id` INT NOT NULL,
   `nome` VARCHAR(50) NOT NULL,
   `cnpj` CHAR(18) NOT NULL,
@@ -26,9 +26,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`modelos`
+-- Table `dev_motors`.`modelos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`modelos` (
+CREATE TABLE IF NOT EXISTS `dev_motors`.`modelos` (
   `id` INT NOT NULL,
   `id_marca` INT NOT NULL,
   `nome` VARCHAR(50) NOT NULL,
@@ -36,16 +36,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`modelos` (
   INDEX `fk_veiculos_marcas_idx` (`id_marca` ASC) VISIBLE,
   CONSTRAINT `fk_veiculos_marcas`
     FOREIGN KEY (`id_marca`)
-    REFERENCES `mydb`.`marcas` (`id`)
+    REFERENCES `dev_motors`.`marcas` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`versoes`
+-- Table `dev_motors`.`versoes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`versoes` (
+CREATE TABLE IF NOT EXISTS `dev_motors`.`versoes` (
   `id` INT NOT NULL,
   `id_modelo` INT NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
@@ -54,16 +54,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`versoes` (
   INDEX `fk_versoes_modelos1_idx` (`id_modelo` ASC) VISIBLE,
   CONSTRAINT `fk_versoes_modelos1`
     FOREIGN KEY (`id_modelo`)
-    REFERENCES `mydb`.`modelos` (`id`)
+    REFERENCES `dev_motors`.`modelos` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`cores`
+-- Table `dev_motors`.`cores`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`cores` (
+CREATE TABLE IF NOT EXISTS `dev_motors`.`cores` (
   `id` INT NOT NULL,
   `nome` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`id`))
@@ -71,30 +71,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`proprietarios`
+-- Table `dev_motors`.`proprietarios`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`proprietarios` (
-  `id` INT NOT NULL,
-  `nome` VARCHAR(50) NOT NULL,
-  `cpf_cnpj` CHAR(18) NOT NULL,
-  `data_nascimento` DATE NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `celular` VARCHAR(45) NOT NULL,
-  `estado` CHAR(2) NOT NULL,
-  `cidade` VARCHAR(45) NOT NULL,
-  `bairro` VARCHAR(45) NOT NULL,
-  `logradouro` VARCHAR(45) NOT NULL,
-  `cep` CHAR(10) NOT NULL,
-  `numero` VARCHAR(45) NOT NULL,
-  `complemento` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`clientes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`clientes` (
+CREATE TABLE IF NOT EXISTS `dev_motors`.`proprietarios` (
   `id` INT NOT NULL,
   `nome` VARCHAR(50) NOT NULL,
   `cpf_cnpj` CHAR(18) NOT NULL,
@@ -113,9 +92,30 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`vendas`
+-- Table `dev_motors`.`clientes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`vendas` (
+CREATE TABLE IF NOT EXISTS `dev_motors`.`clientes` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(50) NOT NULL,
+  `cpf_cnpj` CHAR(18) NOT NULL,
+  `data_nascimento` DATE NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `celular` VARCHAR(45) NOT NULL,
+  `estado` CHAR(2) NOT NULL,
+  `cidade` VARCHAR(45) NOT NULL,
+  `bairro` VARCHAR(45) NOT NULL,
+  `logradouro` VARCHAR(45) NOT NULL,
+  `cep` CHAR(10) NOT NULL,
+  `numero` VARCHAR(45) NOT NULL,
+  `complemento` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dev_motors`.`vendas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `dev_motors`.`vendas` (
   `id` INT NOT NULL,
   `clientes_id` INT NOT NULL,
   `valor` DOUBLE NOT NULL,
@@ -124,16 +124,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`vendas` (
   INDEX `fk_vendas_clientes1_idx` (`clientes_id` ASC) VISIBLE,
   CONSTRAINT `fk_vendas_clientes1`
     FOREIGN KEY (`clientes_id`)
-    REFERENCES `mydb`.`clientes` (`id`)
+    REFERENCES `dev_motors`.`clientes` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`veiculos`
+-- Table `dev_motors`.`veiculos`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`veiculos` (
+CREATE TABLE IF NOT EXISTS `dev_motors`.`veiculos` (
   `id` INT NOT NULL,
   `id_proprietario` INT NOT NULL,
   `id_versao` INT NOT NULL,
@@ -155,31 +155,31 @@ CREATE TABLE IF NOT EXISTS `mydb`.`veiculos` (
   INDEX `fk_veiculos_vendas1_idx` (`id_venda` ASC) VISIBLE,
   CONSTRAINT `fk_veiculos_proprietarios1`
     FOREIGN KEY (`id_proprietario`)
-    REFERENCES `mydb`.`proprietarios` (`id`)
+    REFERENCES `dev_motors`.`proprietarios` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_veiculos_versoes1`
     FOREIGN KEY (`id_versao`)
-    REFERENCES `mydb`.`versoes` (`id`)
+    REFERENCES `dev_motors`.`versoes` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_veiculos_cores1`
     FOREIGN KEY (`id_cor`)
-    REFERENCES `mydb`.`cores` (`id`)
+    REFERENCES `dev_motors`.`cores` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_veiculos_vendas1`
     FOREIGN KEY (`id_venda`)
-    REFERENCES `mydb`.`vendas` (`id`)
+    REFERENCES `dev_motors`.`vendas` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`opcionais`
+-- Table `dev_motors`.`opcionais`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`opcionais` (
+CREATE TABLE IF NOT EXISTS `dev_motors`.`opcionais` (
   `id` INT NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`))
@@ -187,9 +187,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`veiculos_opcionais`
+-- Table `dev_motors`.`veiculos_opcionais`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`veiculos_opcionais` (
+CREATE TABLE IF NOT EXISTS `dev_motors`.`veiculos_opcionais` (
   `id` INT NOT NULL,
   `id_veiculo` INT NOT NULL,
   `id_opcional` INT NOT NULL,
@@ -198,12 +198,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`veiculos_opcionais` (
   INDEX `fk_veiculos_opcionais_opcionais1_idx` (`id_opcional` ASC) VISIBLE,
   CONSTRAINT `fk_veiculos_opcionais_veiculos1`
     FOREIGN KEY (`id_veiculo`)
-    REFERENCES `mydb`.`veiculos` (`id`)
+    REFERENCES `dev_motors`.`veiculos` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_veiculos_opcionais_opcionais1`
     FOREIGN KEY (`id_opcional`)
-    REFERENCES `mydb`.`opcionais` (`id`)
+    REFERENCES `dev_motors`.`opcionais` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
